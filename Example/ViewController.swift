e999feb879e4133d2634e6532d5d340a8cedd9aa
Atharva Vaidya <atharva.vaidya@icloud.com>
2019-11-14 22:49:21 +0530

    Added example project.

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Atharva Vaidya on 11/14/19.
+//  Copyright © 2019 Atharva Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
