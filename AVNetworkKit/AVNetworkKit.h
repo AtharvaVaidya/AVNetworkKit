4c6933b3229141ddbbd93350fb99d1d552e92c30
Atharva Vaidya <atharva.vaidya@icloud.com>
2019-11-14 18:42:20 +0530

    Initial Commit

---
+++ AVNetworkKit/AVNetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  AVNetworkKit.h
+//  AVNetworkKit
+//
+//  Created by Atharva Vaidya on 11/14/19.
+//  Copyright © 2019 Atharva Vaidya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AVNetworkKit.
+FOUNDATION_EXPORT double AVNetworkKitVersionNumber;
+
+//! Project version string for AVNetworkKit.
+FOUNDATION_EXPORT const unsigned char AVNetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVNetworkKit/PublicHeader.h>
+
+
